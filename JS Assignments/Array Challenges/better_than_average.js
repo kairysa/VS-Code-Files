40506a9aefd16f00a276b564f3c09acab288bb28
Kai <noahgardner6@gmail.com>
2022-10-18 19:30:59 -0400

    updated array challenges

--- JS Assignments/Array Challenges/better_than_average.js
+++ JS Assignments/Array Challenges/better_than_average.js
@@ -1,14 +1,21 @@
-var numList = [6, 8, 3, 10, -2, 9, 5];
+var arr = [6, 8, 3, 10, -2, 9, 5];
 var sum = 0;
 var avg = 0;
 var count = 0;
 
-function betterThanAverage(){
-    for(i=0; i<numList.length; i++){
-        sum += numList[i];
+function betterThanAverage(arr){
+    for(i=0; i<arr.length; i++){
+        sum += arr[i];
     }
     avg = sum/7;
-
+    for(i=0; i<arr.length; i++){
+        if (arr[i] > avg){
+            count += 1;
+        }
+    }
+    return count;
 }
 
-betterThanAverage();
+betterThanAverage(arr);
+
+console.log(count);
