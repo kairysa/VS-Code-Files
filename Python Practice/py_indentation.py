d704ae4605a166be7d1862cbe5adca77ad0b7652
Kai <59454833+kairysa@users.noreply.github.com>
2022-10-29 17:14:03 -0400

    Delete py_indentation.py

--- Python Practice/py_indentation.py
+++
@@ -1,9 +0,0 @@
-x = 10
-if x > 50:
-    print("bigger than 50")
-else:
-    print("smaller than 50")
-empty_dict = {}
-new_person = {'name':'John','age': 44, 'weight': 160.2, 'has_glasses': False}
-w = new_person
-print(w)
