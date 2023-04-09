44860fa948d8b8382c487a84b0b8c8b19d835f78
Noah Gardner <Noahgardner6@gmail.com>
2023-04-09 15:35:36 -0400

    Full Stack Practice

--- MERN Part Time/Notes and Lectures/Mongo and Express Set Up/server/models/tvshow.model.js
+++ MERN Part Time/Notes and Lectures/Mongo and Express Set Up/server/models/tvshow.model.js
@@ -2,21 +2,36 @@ const mongoose = require('mongoose');
 
 const TvShow = new mongoose.Schema({
     title: {
-      type: String
+      type: String,
+      required:[true, 'Title is required'],
+      minLength:[3, 'Title must be 3 or more characters'],
+      maxLength:[50,'Title is too long']
     },
     releaseYear: {
-      type: Number
+      type: Number,
+      min: [1920, "No years before 1920 allowed"]
     },
     network: {
-      type: String
+      type: String,
+      required:[true, 'Network is required'],
+      minLength:[3, 'Network must be 3 or more characters'],
+      maxLength:[50,'Network is too long']
     },
     creator: {
-      type: String
+      type: String,
+      required:[true, 'Creator is required'],
+      minLength:[3, 'Creator must be 3 or more characters'],
+      maxLength:[50,'Creator is too long']
     },
     genre: {
-      type: String
+      type: String,
+      required:[true, 'Creator is required'],
+      minLength:[3, 'Creator must be 3 or more characters'],
+      maxLength:[50,'Title is too long']
     }
-});
+
+    // ! ADD TIMESTAMPS //
+}, {timestamps:true});
 
 const Show = mongoose.model('Show', TvShow);
 
