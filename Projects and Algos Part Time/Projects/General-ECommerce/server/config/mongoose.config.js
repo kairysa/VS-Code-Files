53287baa5d4efd4063b8e13f69e92e5eeb8cb363
Noah Gardner <Noahgardner6@gmail.com>
2023-05-20 17:53:04 -0400

    First commit creating application using React and folder structure for server side

---
+++ Projects and Algos Part Time/Projects/General-ECommerce/server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+//This will create a database if one doesn't already exist (no need for mongo shell!):
+mongoose.connect("mongodb://127.0.0.1:27017/beauty-ecommerce", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
